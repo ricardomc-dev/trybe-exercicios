a51110204b840f023b73a9ccbca98eeefebbaa9b
Ricardo Carvalho <ricardomc.dev@gmail.com>
2021-09-06 18:39:40 -0300

    Finalizado primeiro exercicio 5-2

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script5-2.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script5-2.js
@@ -17,5 +17,38 @@ let item7 = document.querySelector('#elementoOndeVoceEsta').nextElementSibling;
 
 let item8 = document.querySelector('#pai').lastChild.previousElementSibling;
 
-let item9 = document.createElement('div');
-let texto9 = item9.innerHTML = 'Criado um irmão para elementoOndeVoceEsta ';
+//Crie um irmão para elementoOndeVoceEsta
+let irmao = 'Irmao criado para elementoOndeVoceEsta';
+let elemCriado = document.createElement('section');
+elemCriado.innerText = irmao;
+let elemRec = document.querySelector('#pai');
+elemRec.appendChild(elemCriado);
+elemCriado.id = 'quintoFilho';
+
+//Crie um filho para elementoOndeVoceEsta 
+let filho = 'Filho criado para elementoOndeVoceEsta';
+let elemCriado2 = document.createElement('p');
+elemCriado2.innerText = filho;
+let elemRec2 = document.querySelector('#elementoOndeVoceEsta');
+elemRec2.appendChild(elemCriado2);
+elemCriado2.id = 'terceiroFilhoDoFilho'
+
+//Crie um filho para primeiroFilhoDoFilho .
+let neto = 'filho do filho criado';
+let elemCriado3 = document.createElement('h4');
+elemCriado3.innerText = neto;
+let elemRec3 = document.querySelector('#primeiroFilhoDoFilho');
+elemRec3.appendChild(elemCriado3);
+elemCriado3.id = 'filhoDoPrimeiroFilhoDoFilho'
+
+//A partir desse filho criado, acesse terceiroFilho 
+let item9 = document.querySelector('#filhoDoPrimeiroFilhoDoFilho').parentElement.parentElement.nextElementSibling
+console.log(item9)
+
+// Remova todos os elementos filhos de paiDoPai exceto pai , elementoOndeVoceEsta e primeiroFilhoDoFilho 
+let elemPai = document.querySelector('#pai');
+let elemRemove1 = elemPai.lastElementChild;
+elemPai.removeChild(elemRemove1)
+
+
+console.log(elemRemove1);
