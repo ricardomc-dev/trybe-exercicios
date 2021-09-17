06f851dcbaafc8f571f307baba78d72dd27e21fb
Ricardo Carvalho <ricardomc.dev@gmail.com>
2021-09-17 15:10:13 -0300

    Criação do Form e aplicação do Framework bulma

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,71 +1,65 @@
-let states = [
-  { 'AC': 'Acre' },
-  { 'AL': 'Alagoas' },
-  { 'AP': 'Amapá' },
-  { 'AM': 'Amazonas' },
-  { 'BA': 'Bahia' },
-  { 'CE': 'Ceará' },
-  { 'DF': 'Distrito Federal' },
-  { 'ES': 'Espírito Santo' },
-  { 'GO': 'Goías' },
-  { 'MA': 'Maranhão' },
-  { 'MT': 'Mato Grosso' },
-  { 'MS': 'Mato Grosso do Sul' },
-  { 'MG': 'Minas Gerais' },
-  { 'PA': 'Pará' },
-  { 'PB': 'Paraíba' },
-  { 'PR': 'Paraná' },
-  { 'PE': 'Pernambuco' },
-  { 'PI': 'Piauí' },
-  { 'RJ': 'Rio de Janeiro' },
-  { 'RN': 'Rio Grande do Norte' },
-  { 'RS': 'Rio Grande do Sul' },
-  { 'RO': 'Rondônia' },
-  { 'RR': 'Roraíma' },
-  { 'SC': 'Santa Catarina' },
-  { 'SP': 'São Paulo' },
-  { 'SE': 'Sergipe' },
-  { 'TO': 'Tocantins' },
-];
-//console.log(states[3])
-// let states =
-//   {'AC': 'Acre', 
-//    'AL': 'Alagoas',
-//    'AP': 'Amapá',
-//    'AM': 'Amazonas',
-//    'BA': 'Bahia',
-//    'CE': 'Ceará',
-//    'DF': 'Distrito Federal',
-//    'ES': 'Espírito Santo',
-//    'GO': 'Goías',
-//    'MA': 'Maranhão',
-//    'MT': 'Mato Grosso',
-//    'MS': 'Mato Grosso do Sul',
-//    'MG': 'Minas Gerais',
-//    'PA': 'Pará',
-//    'PB': 'Paraíba',
-//    'PR': 'Paraná',
-//    'PE': 'Pernambuco',
-//    'PI': 'Piauí',
-//    'RJ': 'Rio de Janeiro',
-//    'RN': 'Rio Grande do Norte',
-//    'RS': 'Rio Grande do Sul',
-//    'RO': 'Rondônia',
-//    'RR': 'Roraíma',
-//    'SC': 'Santa Catarina',
-//    'SP': 'São Paulo' ,
-//    'SE': 'Sergipe' ,
-//    'TO': 'Tocantins' ,
-//   };
-console.log(states[1])
+// let states = [
+//   { 'AC': 'Acre' },
+//   { 'AL': 'Alagoas' },
+//   { 'AP': 'Amapá' },
+//   { 'AM': 'Amazonas' },
+//   { 'BA': 'Bahia' },
+//   { 'CE': 'Ceará' },
+//   { 'DF': 'Distrito Federal' },
+//   { 'ES': 'Espírito Santo' },
+//   { 'GO': 'Goías' },
+//   { 'MA': 'Maranhão' },
+//   { 'MT': 'Mato Grosso' },
+//   { 'MS': 'Mato Grosso do Sul' },
+//   { 'MG': 'Minas Gerais' },
+//   { 'PA': 'Pará' },
+//   { 'PB': 'Paraíba' },
+//   { 'PR': 'Paraná' },
+//   { 'PE': 'Pernambuco' },
+//   { 'PI': 'Piauí' },
+//   { 'RJ': 'Rio de Janeiro' },
+//   { 'RN': 'Rio Grande do Norte' },
+//   { 'RS': 'Rio Grande do Sul' },
+//   { 'RO': 'Rondônia' },
+//   { 'RR': 'Roraíma' },
+//   { 'SC': 'Santa Catarina' },
+//   { 'SP': 'São Paulo' },
+//   { 'SE': 'Sergipe' },
+//   { 'TO': 'Tocantins' },
+// ];
 
-//function createStateSelect() {
+function createStateSelect() {
   const getSelect = document.querySelector('select');
-  const state = document.createElement('option');
-  for (let i = 0; i < states.length; i += 1) {
-    state.innerText = states[i];
+  let stateOptions = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+  
+  for (let i = 0; i < stateOptions.length; i += 1) {
+    const state = document.createElement('option');
+    state.innerText = stateOptions[i];
+    state.value = stateOptions[i];
     getSelect.appendChild(state)
   }
-  console.log(state)
-// }
-// createStateSelect();
+
+  window.onload = function () {
+    createStateSelect();
+    let submitButton = document.querySelector('.submit-button');
+    submitButton.addEventListener('click', handleSubmit);
+  
+    let clearButton = document.querySelector('.clear-button');
+    clearButton.addEventListener('click', clearFields)
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  
+    let validation = validateData();
+  
+    clearDivs();
+    
+    if(validation.errorQtd === 0){
+      renderData();
+    } else {
+      validation.messages.unshift('Dados Inválidos')
+  
+      renderErrorMessages(validation.messages)
+    }
+  }
