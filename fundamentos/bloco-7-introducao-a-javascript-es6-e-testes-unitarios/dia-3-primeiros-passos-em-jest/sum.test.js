52708071212bc6157e746765750f8d8341603970
Ricardo Carvalho <ricardomc.dev@gmail.com>
2021-09-29 14:01:00 -0300

    Elaborado teste função myFizzBuzz

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,6 +1,6 @@
 // const { it, expect } = require('@jest/globals');
 // const { describe } = require('yargs');
-const { sum, myRemove }  = require('./sum');
+const { sum, myRemove, myFizzBuzz }  = require('./sum');
 
 describe("teste a função sum", () => {
 
@@ -21,6 +21,29 @@ describe("teste a função myRemove", () => {
 
   it('A função deve retornar um array removendo o item do parâmetro', () => {
     expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
-    
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
   })
+  
+})
+
+
+describe("teste a função myFizzbuzz", () => {
+
+  it('A função recebe 15 e retorna fizzbuzz', () => {
+    expect(myFizzBuzz(15)).toMatch('fizzbuzz')
+  })
+  it('A função recebe 3 e retorna fizz', () => {
+    expect(myFizzBuzz(3)).toMatch('fizz')
+  })
+  it('A função recebe 5 e retorna buzz', () => {
+    expect(myFizzBuzz(5)).toMatch('buzz')
+  })
+  it('A função recebe 2 e retorna 2', () => {
+    expect(myFizzBuzz(2)).toEqual(2)
+  })
+  it('A função recebe a e retorna false', () => {
+    expect(myFizzBuzz('a')).toBeFalsy()
+  })
+
 })
