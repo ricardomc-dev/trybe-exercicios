686374b2e874e8f72b30e8001fa80c73f35078e1
Ricardo Carvalho <ricardomc.dev@gmail.com>
2021-10-04 19:09:44 -0300

    Finalizados Exerc 1, 2 e 3

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/scripts-8.1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/scripts-8.1.js
@@ -34,3 +34,36 @@ const newEmployees = (callback) => {
 };
 
 console.log(newEmployees(users));
+
+/*2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").*/
+
+const numberChecker = (myNumber, number) => myNumber === number
+
+const sorteio = (myNumber, callback) => {
+  const number = Math.floor(Math.random()* 5 + 1);
+  return callback(myNumber, number) ? 'Parabéns você ganhou' : 'Tente novamente';
+}
+
+console.log(sorteio(1, numberChecker))
+
+
+/*3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.*/
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const answersChecker = (array1, array2) => {
+  let pontos = 0;
+  for (let index = 0; index < RIGHT_ANSWERS.length; index += 1) {
+    if (array2[index] === 'N.A') pontos += 0;
+    if (array1[index] === array2[index]) pontos += 1;
+    if (array1[index] !== array2[index]) pontos -= 0.5;
+  }
+  return pontos;
+}
+const gabarito = (gabaritoCorreto, gabaritoEstudante, callback) => {
+ return callback(gabaritoCorreto, gabaritoEstudante);
+};
+
+console.log(gabarito(RIGHT_ANSWERS, STUDENT_ANSWERS, answersChecker))
