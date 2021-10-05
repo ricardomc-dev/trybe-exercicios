04302685e78b9ea12340ab140c17b581917ccf39
Ricardo Carvalho <ricardomc.dev@gmail.com>
2021-10-05 12:29:28 -0300

    Resolvido Exec .some, .every e .sort

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/scripts-8.2.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/scripts-8.2.js
@@ -46,3 +46,57 @@ function findMusic(id) {
 }
 
 console.log(findMusic('31031685'))
+
+//1 - Escreva uma função que dado um array de nomes e um nome retorne true se ele estiver contido e caso contrário, retorne false , use some ;
+
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => {
+  return arr.some(element => element === name);
+}
+
+console.log(hasName(names, 'Ana'))
+
+// 2 - Escreva uma função que dado um array de pessoas e uma idade mínima retorne true se todas tiverem a idade maior ou igual a mínima e caso contrário false , use every ;
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => {
+  return arr.every(element => element.age >= minimumAge);
+}
+
+console.log(verifyAges(people, 18));
+
+// 1 - Utilize a sort para ordenar o array pela idade das pessoas em ordem crescente .
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+people.sort((a, b) => a.age - b.age);
+
+console.log(people);
+
+//2 - Modifique o sort do exercício anterior para que ordene o array pela idade das pessoas em ordem decrescente .
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+people.sort((a, b) => b.age - a.age);
+
+console.log(people);
