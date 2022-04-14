86bfdce214d23ea106cc319ddd2970fd392c7714
Ricardo Carvalho <ricardomc.dev@gmail.com>
2022-04-13 21:59:04 -0300

    Exercicios intro node

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/hello-world/index.js
@@ -0,0 +1,10 @@
+const readline = require('readline-sync');
+
+const name = readline.question('What is your name? ');
+const age = readline.questionInt('How old are you? ');
+
+console.log(`Hello, ${name}! You are ${age} years old!`);
+
+const favFood = readline.question('What is your favorite food? ');
+
+console.log(`Oh, ${name} loves ${favFood}`);
