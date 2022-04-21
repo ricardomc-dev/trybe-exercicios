fc10d1c4ccef233ca66bfc60d09c404a171dd9e2
Ricardo Carvalho <ricardomc.dev@gmail.com>
2022-04-21 14:57:00 -0300

    Resolvido Exercicio-3 | Refatoração Exercicio-2

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicios/exercicio-1.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicios/exercicio-1.js
@@ -1,4 +1,4 @@
-function calcNumeros (num1, num2, num3) {
+function calcNumbers (num1, num2, num3) {
   return new Promise((resolve, reject) => {
     if (typeof num1 !== 'number' || typeof num2 !== 'number' || typeof num3 !== 'number') {
       reject(new Error("Informe apenas números"))
@@ -9,8 +9,8 @@ function calcNumeros (num1, num2, num3) {
   })
 }
 
-// calcNumeros("teste", 1, 5)
+// calcNumbers("teste", 1, 5)
 //   .then(result => console.log(result))
 //   .catch(err => console.log(`erro: ${err.message}`));
 
-module.exports = calcNumeros;
+module.exports = calcNumbers;
--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicios/exercicio-3.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicios/exercicio-3.js
@@ -4,14 +4,17 @@ function getRandomNumber() {
   return Math.floor(Math.random() * 100 + 1);
 }
 
-function callCalcNumbers() {
+async function callCalcNumbers() {
   const randomNumbers = Array.from({length: 3}).map(getRandomNumber);
 
   console.log(randomNumbers);
 
-  calcNumbers(...randomNumbers)
-    .then(result => console.log(result))
-    .catch(err => console.log(`erro: ${err.message}`));
+  try {
+    const result = await calcNumbers(...randomNumbers)
+    console.log(result);
+  } catch (err) {
+    console.log(`erro: ${err.message}`);
+  }
 }
 
 callCalcNumbers();
