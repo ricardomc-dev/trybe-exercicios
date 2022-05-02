7b8ae44e50b227c61243d5af01f0a42f3ce9289e
Ricardo Carvalho <ricardomc.dev@gmail.com>
2022-05-02 16:00:48 -0300

    finalizado Authors Exercices

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 
-const authors = require('./models/Author');
+const Author = require('./models/Author');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (_req, res) => {
+app.get("/authors", async (_req, res) => {
+  const authors = await Author.getAll();
   res.status(200).json(authors);
 })
 
 app.listen(PORT, () => {
   console.log(`Ouvindo a porta ${PORT}`);
 });
+
+
+
